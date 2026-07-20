d802abf11214ef2cac4b26f8b95f3dc25e7320fb
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-20 16:50:55 -0300

    TS EM PROCESSO

--- ts/cadastro.ts
+++ ts/cadastro.ts
@@ -1,1 +1,6 @@
-const formCadastro = document.querySelector('#') as HTMLFormElement
+const formCadastro = document.querySelector('#form-row') as HTMLFormElement
+const nomeInpt = document.querySelector('#client-id') as HTMLInputElement
+const nomput = document.querySelector('#client-id') as HTMLInputElement
+const nomeut = document.querySelector('#client-id') as HTMLInputElement
+const nomeIput = document.querySelector('#client-id') as HTMLInputElement
+const nomeIut = document.querySelector('#client-id') as HTMLInputElement
